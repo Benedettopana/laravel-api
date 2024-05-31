10b9f6d5b5e3167017bac7b8cb869eec2b3c0362
Benedettopana <benedetto.pana@gmail.com>
2024-05-31 15:14:19 +0200

    first commit

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::with('type', 'technologies')->paginate(15);
+        return response()->json($projects);
+    }
+}
